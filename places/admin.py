d6b525ea897a76136fe27eb8c5b5434c6cc28c05
olenevoid <mazekunov@gmail.com>
2026-04-08 16:23:11 +0500

    Add inline images field for places in admin panel

--- places/admin.py
+++ places/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
 
-from .models import Place, Image
+from .models import Image, Place
 
-admin.site.register(Place)
-admin.site.register(Image)
+
+class ImageInline(admin.TabularInline):
+    model = Image
+    extra = 1
+
+
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    inlines = [ImageInline]
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    pass
