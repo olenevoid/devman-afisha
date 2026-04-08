1287740fd5b006f3d435382a4c669eaa20e4162b
olenevoid <mazekunov@gmail.com>
2026-04-08 14:48:28 +0500

    Move data from template to view

--- places/views.py
+++ places/views.py
@@ -1,6 +1,17 @@
+import json
+
+from django.conf import settings
 from django.shortcuts import render
 
 
 def show_index(request):
     print("Кто-то зашёл на главную!")
-    return render(request, "index.html")
+
+    with open(
+        settings.BASE_DIR / "static" / "places.json", encoding="utf-8"
+    ) as f:
+        places_geojson = json.load(f)
+
+    return render(
+        request, "index.html", {"places_geojson": json.dumps(places_geojson)}
+    )
