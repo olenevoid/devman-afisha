c8988f1a4bf993f2559f62ba480379392ccd9d9e
olenevoid <mazekunov@gmail.com>
2026-04-09 14:18:55 +0500

    Use json_script

--- places/views.py
+++ places/views.py
@@ -27,11 +27,9 @@ def show_index(request):
 
     features = [serialize_place(place) for place in Place.objects.all()]
 
-    places_geojson = json.dumps(
-        {"type": "FeatureCollection", "features": features}
-    )
-
-    context = {"places_geojson": places_geojson}
+    context = {
+        "places_geojson": {"type": "FeatureCollection", "features": features}
+    }
 
     return render(request, "index.html", context)
 
